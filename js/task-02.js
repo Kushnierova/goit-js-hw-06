e5a396ee78a43dc4610d338aa4b43b66875f7b8f
Kushnierova <Katerynakushnierova@gmail.com>
2023-04-28 19:52:23 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const arrayOfIngredients = ingredients.map((ingredient) => {
+  const listOfIngredients = document.createElement("li");
+  console.log(listOfIngredients);
+  listOfIngredients.textContent = ingredient;
+  listOfIngredients.classList.add("item");
+  return listOfIngredients;
+});
+
+const listEl = document.querySelector("#ingredients");
+listEl.prepend(...arrayOfIngredients);
