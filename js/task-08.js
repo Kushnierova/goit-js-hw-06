61b84250f4d0a2e02e55ffb5063a40e96330bf99
Kushnierova <Katerynakushnierova@gmail.com>
2023-05-01 12:24:22 +0200

    task-08
    тимчасове збереження

--- js/task-08.js
+++ js/task-08.js
@@ -1,25 +1,37 @@
-const loginForm = document.querySelector(".login-form");
+// const loginForm = document.querySelector(".login-form");
 
-loginForm.addEventListener("submit", formSubmit )
-function formSubmit(e) {
+// loginForm.addEventListener("submit", formSubmit )
+// function formSubmit(e) {
 
-    e.preventDefalt();
+//     e.preventDefalt();
 
-    const {
-    elements: {email, password},
-} = e.currentTarget;
+//     const {
+//     elements: {email, password},
+// } = e.currentTarget;
 
-if (email.value === "" || password.value === "") {
-    return alert("Для входу всі поля мають бути заповнені");
+// if (email.value === "" || password.value === "") {
+//     return alert("Для входу всі поля мають бути заповнені");
+// }
+
+// // 4
+// const nameForm = {
+//     [email.name] : email.value,
+//     [password.name] : password.value,
+// };
+// console.log(nameForm);
+
+// e.target.reset()
+
+// }
+
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (e) =>{
+e.preventDefault()
+// console.log(e)
+// console.log(e.target.elements.email.value)
+const data = {
+    email: e.target.elements.email.value,
+    password: e.target.elements.password.value,
 }
-
-// 4
-const nameForm = {
-    [email.name] : email.value,
-    [password.name] : password.value,
-};
-console.log(nameForm);
-
-e.target.reset()
-
-}
+console.log(data)
+})
