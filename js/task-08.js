d4fb0e2768865517500c5882c53ee0defa3c9dd7
Kushnierova <Katerynakushnierova@gmail.com>
2023-05-01 14:32:47 +0200

    task-08
    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,40 +1,20 @@
-// const loginForm = document.querySelector(".login-form");
-
-// loginForm.addEventListener("submit", formSubmit )
-// function formSubmit(e) {
-
-//     e.preventDefalt();
-
-//     const {
-//     elements: {email, password},
-// } = e.currentTarget;
-
-// if (email.value === "" || password.value === "") {
-//     return alert("Для входу всі поля мають бути заповнені");
-// }
-
-// // 4
-// const nameForm = {
-//     [email.name] : email.value,
-//     [password.name] : password.value,
-// };
-// console.log(nameForm);
-
-// e.target.reset()
-
-// }
-
 const form = document.querySelector(".login-form");
 form.addEventListener("submit", (e) =>{
 e.preventDefault();
-// console.log(e)
-// console.log(e.target.elements.email.value)
-const data = {
-    email: e.target.elements.email.value,
-    password: e.target.elements.password.value,
-}
-// console.log(data)
+// console.log(e);
+
+    const {
+    elements: {email, password},
+} = e.target;
+
 if(email.value === "" || password.value ===""){
 return alert("Всі поля мають бути заповнені");
 }
+
+// console.log(e.target.elements.email.value);
+const data = {
+    email: email.value,
+    password: password.value,
+}
+console.log(data);
 })
