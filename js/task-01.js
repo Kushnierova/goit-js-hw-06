7326af9b92b2672180beae7ab0d605a03cc6fd15
Kushnierova <Katerynakushnierova@gmail.com>
2023-08-18 21:18:47 +0200

    21:18

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,20 @@
-const items = document.querySelectorAll('.item');
-console.log('Number of categories:', items.length);
+// const items = document.querySelectorAll('.item');
+// console.log('Number of categories:', items.length);
 
-items.forEach((element) => 
-console.log(
+// items.forEach((element) =>
+// console.log(
+//     `Category: ${element.firstElementChild.textContent}`,
+//     `Elements: ${element.lastElementChild.children.length}`,
+// )
+// );
+
+const items = document.querySelectorAll(`.item`);
+console.log('Number of categories:', items.length);
+console.log(items);
+
+items.forEach(element =>
+  console.log(
     `Category: ${element.firstElementChild.textContent}`,
-    `Elements: ${element.lastElementChild.children.length}`,
-)
+    `Elements: ${element.lastElementChild.children.length}`
+  )
 );
